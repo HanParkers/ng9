acf34f7307855a05b93442068116fc761b4abd93
HanParkers <han.parker,.99@outlook.com>
2019-11-10 13:17:55 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <p>{{ title }}</p>
+      <p>Some clean app</p>
   `
 })
 export class AppComponent {
-  title = 'ng9';
 }
