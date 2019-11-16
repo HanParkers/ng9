c41f0366cf4e63112e5184e04e911e9936e4a393
HanParkers <han.parker,.99@outlook.com>
2019-11-16 16:00:53 +0200

    add @ngrx/store. Test ngrx state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import {decrement, increment, reset} from './counter.store';
 
 @Component({
   selector: 'app-root',
   template: `
-      <p>Some clean app</p>
+      <button id="increment" (click)="increment()" type="button">Increment</button>
+
+      <div>Current count {{ count$ | async }}</div>
+
+      <button id="decrement" (click)="decrement()" type="button">Decrement</button>
+      <button id="reset" (click)="reset()" type="button">Reset</button>
   `
 })
 export class AppComponent {
+
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
+
+import {CounterReducer} from './counter.store';
 
 import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({count: CounterReducer})],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
